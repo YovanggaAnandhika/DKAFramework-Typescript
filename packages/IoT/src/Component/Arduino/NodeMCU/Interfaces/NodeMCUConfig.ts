b6da2d81a76cf752372ce327a3d4a44161a55b18
Yovangga Anandhika <dka.tech.dev@gmail.com>
2023-08-05 01:38:24 +0800

    Refactor Interface NodeMCU

--- packages/IoT/src/Component/Arduino/NodeMCU/Interfaces/NodeMCUConfig.ts
+++ packages/IoT/src/Component/Arduino/NodeMCU/Interfaces/NodeMCUConfig.ts
@@ -1,24 +1,62 @@
 import {BoardOption} from "johnny-five";
+import {MethodNodeMCU} from "../index";
 
 
-export interface NodeMCUConfig extends BoardOption {
+export type MODE_USB = "USB_MODE";
+export type MODE_WIFI = "WIFI_MODE";
 
+export type ALL_MODE = MODE_WIFI | MODE_USB;
+export const MODE_USB: MODE_USB = "USB_MODE";
+export const MODE_WIFI : MODE_WIFI = "WIFI_MODE";
+
+export interface NodeMCUConfigWifi extends BoardOption {
+    mode : MODE_WIFI,
+    host ?: string | undefined,
+    port ?: number,
+    repl ?: boolean,
+    onConnect ?: () => void | Promise<void>,
+    onReady ?: (MethodNodeMCU) => void | Promise<void>,
+    onError ?: () => void | Promise<void>,
+    onFail ?: () => void | Promise<void>,
+    onClose ?: () => void | Promise<void>
 }
+export interface NodeMCUConfigUSB extends BoardOption {
+    mode : MODE_USB,
+    port ?: string,
+    repl ?: boolean,
+    onConnect ?: () => void | Promise<void>,
+    onReady ?: (method ?: typeof MethodNodeMCU | undefined) => void | Promise<void>,
+    onError ?: (error ?: Error | undefined) => void | Promise<void>,
+    onFail ?: () => void | Promise<void>,
+    onClose ?: () => void | Promise<void>
+}
+export type NodeMCUConfig = NodeMCUConfigWifi | NodeMCUConfigUSB;
 
-export type LED_MODE_NORMAL = "LED_MODE_NORMAL";
-export type LED_MODE_TOGGLE = "LED_MODE_TOGGLE";
 
-export interface NodeMCULedMethodNormal {
+export type BOARD_STATE_IDLE = "BOARD_STATE_IDLE";
+export type BOARD_STATE_READY = "BOARD_STATE_READY";
+export type BOARD_STATE_CLOSE = "BOARD_STATE_CLOSE";
+export type BOARD_STATE_CONNECT = "BOARD_STATE_CONNECT";
+export type BOARD_STATE_FAIL = "BOARD_STATE_FAIL";
+export type BOARD_STATE_ERROR = "BOARD_STATE_ERROR";
+
+export type ALL_STATE = BOARD_STATE_IDLE | BOARD_STATE_READY | BOARD_STATE_CONNECT | BOARD_STATE_CLOSE | BOARD_STATE_FAIL | BOARD_STATE_ERROR;
+
+export const BOARD_STATE_IDLE : BOARD_STATE_IDLE = "BOARD_STATE_IDLE";
+export const BOARD_STATE_READY : BOARD_STATE_READY = "BOARD_STATE_READY";
+export const BOARD_STATE_CONNECT : BOARD_STATE_CONNECT = "BOARD_STATE_CONNECT";
+export const BOARD_STATE_CLOSE : BOARD_STATE_CLOSE = "BOARD_STATE_CLOSE";
+export const BOARD_STATE_FAIL : BOARD_STATE_FAIL = "BOARD_STATE_FAIL";
+export const BOARD_STATE_ERROR : BOARD_STATE_ERROR = "BOARD_STATE_ERROR";
+
+export interface NodeMCULedMethod {
     pin ?: number,
-    mode ?: LED_MODE_NORMAL,
+    reverse ?: boolean,
     state ?: boolean
 }
 
-export interface NodeMCULedMethodToggle {
+export interface NodeMCURelayMethod {
     pin ?: number,
-    mode ?: LED_MODE_TOGGLE,
     reverse ?: boolean,
-    bounceInterval ?: number
+    state ?: boolean
 }
-
-export type NodeMCULedMethod = NodeMCULedMethodNormal | NodeMCULedMethodToggle
