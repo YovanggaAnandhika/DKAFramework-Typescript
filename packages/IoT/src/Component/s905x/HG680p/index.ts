3c150c8932a320be6fde135c635a621e2d330654
Yovangga Anandhika <dka.tech.dev@gmail.com>
2024-03-07 18:15:36 +0800

    Add Compabilities Port GPIO

--- packages/IoT/src/Component/s905x/HG680p/index.ts
+++ packages/IoT/src/Component/s905x/HG680p/index.ts
@@ -8,16 +8,19 @@ export class HG680p {
 
     private gpioPath = "/sys/class/gpio";
     private isLinux = false;
+    private versionDevice : "v1" | "v2" = "v2"
     private isGpioExist = false;
 
-    constructor() {
+    constructor(version : "v1" | "v2" = "v2") {
+        this.versionDevice = version;
         this.isLinux = (os.platform() === "linux");
         this.isGpioExist = (this.isLinux) ? (fs.existsSync(this.gpioPath)) : false;
     }
 
     lan(state : "on" | "off" | "dis" | "warn"){
-        let pin_on= 510;
-        let pin_off= 506;
+
+        let pin_on= (this.versionDevice === "v2") ? 510 : 482;
+        let pin_off= (this.versionDevice === "v2") ? 506 : 481;
         //######################################################################
         if (!this.isLinux)
             throw new Error("platform not linux");
@@ -60,8 +63,9 @@ export class HG680p {
     }
 
     power(state : "on" | "off" | "dis" | "warn"){
-        let pin_on= 425;
-        let pin_off= 426;
+        //######################################################################
+        let pin_on= (this.versionDevice === "v2") ? 425 : 483;
+        let pin_off= (this.versionDevice === "v2") ? 426 : 484;
         //######################################################################
         if (!this.isLinux)
             throw new Error("platform not linux");
@@ -98,6 +102,33 @@ export class HG680p {
                 case "dis" :
                     execSync(`echo 0 > "${path.join(this.gpioPath,`/gpio${pin_on}`,`/value`)}" `)
                     execSync(`echo 0 > "${path.join(this.gpioPath,`/gpio${pin_off}`,`/value`)}" `)
+                    break;
+            }
+        }catch (e) {}
+    }
+
+    ir(state : "on" | "off"){
+        let pin= (this.versionDevice === "v2") ? 507 : 480;
+        //######################################################################
+        if (!this.isLinux)
+            throw new Error("platform not linux");
+        if (!this.isGpioExist)
+            throw new Error("system not support gpio");
+        //######################################################################
+        try {
+            if (!fs.existsSync(`${path.join(this.gpioPath,`gpio${pin}`)}`)) {
+                execSync(`echo ${pin} > ${this.gpioPath}/export`);
+                execSync(`echo out > ${path.join(this.gpioPath,`/gpio${pin}`,`/direction`)}`);
+            }else{
+                execSync(`echo out > ${path.join(this.gpioPath,`/gpio${pin}`,`/direction`)}`);
+            }
+            //######################################################################
+            switch (state) {
+                case "on" :
+                    execSync(`echo 1 > "${path.join(this.gpioPath,`/gpio${pin}`,`/value`)}" `)
+                    break;
+                case "off" :
+                    execSync(`echo 0 > "${path.join(this.gpioPath,`/gpio${pin}`,`/value`)}" `)
                     break;
             }
         }catch (e) {}
