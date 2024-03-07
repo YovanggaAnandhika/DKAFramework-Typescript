5203a5f0bb0e15aa2621b5424466cb88e6bd528c
Yovangga Anandhika <dka.tech.dev@gmail.com>
2024-03-07 17:28:20 +0800

    Add Try catch

--- packages/IoT/src/Component/s905x/HG680p/index.ts
+++ packages/IoT/src/Component/s905x/HG680p/index.ts
@@ -24,37 +24,39 @@ export class HG680p {
         if (!this.isGpioExist)
             throw new Error("system not support gpio");
         //######################################################################
-        if (!fs.existsSync(`${path.join(this.gpioPath,`gpio${pin_on}`)}`)) {
-            execSync(`echo ${pin_on} > ${this.gpioPath}/export`);
-            execSync(`echo out > ${path.join(this.gpioPath,`/gpio${pin_on}`,`/direction`)}`);
-        }else{
-            execSync(`echo out > ${path.join(this.gpioPath,`/gpio${pin_on}`,`/direction`)}`);
-        }
-        if (!fs.existsSync(`${path.join(this.gpioPath,`gpio${pin_off}`)}`)){
-            execSync(`echo ${pin_off} > ${this.gpioPath}/export`);
-            execSync(`echo out > ${path.join(this.gpioPath,`/gpio${pin_off}`,`/direction`)}`);
-        }else{
-            execSync(`echo out > ${path.join(this.gpioPath,`/gpio${pin_off}`,`/direction`)}`);
-        }
-        //######################################################################
-        switch (state) {
-            case "on" :
-                execSync(`echo 0 > "${path.join(this.gpioPath,`/gpio${pin_off}`,`/value`)}" `)
-                execSync(`echo 1 > "${path.join(this.gpioPath,`/gpio${pin_on}`,`/value`)}" `)
-                break;
-            case "off" :
-                execSync(`echo 0 > "${path.join(this.gpioPath,`/gpio${pin_on}`,`/value`)}" `)
-                execSync(`echo 1 > "${path.join(this.gpioPath,`/gpio${pin_off}`,`/value`)}" `)
-                break;
-            case "warn" :
-                execSync(`echo 1 > "${path.join(this.gpioPath,`/gpio${pin_on}`,`/value`)}" `)
-                execSync(`echo 1 > "${path.join(this.gpioPath,`/gpio${pin_off}`,`/value`)}" `)
-                break;
-            case "dis" :
-                execSync(`echo 0 > "${path.join(this.gpioPath,`/gpio${pin_on}`,`/value`)}" `)
-                execSync(`echo 0 > "${path.join(this.gpioPath,`/gpio${pin_off}`,`/value`)}" `)
-                break;
-        }
+        try {
+            if (!fs.existsSync(`${path.join(this.gpioPath,`gpio${pin_on}`)}`)) {
+                execSync(`echo ${pin_on} > ${this.gpioPath}/export`);
+                execSync(`echo out > ${path.join(this.gpioPath,`/gpio${pin_on}`,`/direction`)}`);
+            }else{
+                execSync(`echo out > ${path.join(this.gpioPath,`/gpio${pin_on}`,`/direction`)}`);
+            }
+            if (!fs.existsSync(`${path.join(this.gpioPath,`gpio${pin_off}`)}`)){
+                execSync(`echo ${pin_off} > ${this.gpioPath}/export`);
+                execSync(`echo out > ${path.join(this.gpioPath,`/gpio${pin_off}`,`/direction`)}`);
+            }else{
+                execSync(`echo out > ${path.join(this.gpioPath,`/gpio${pin_off}`,`/direction`)}`);
+            }
+            //######################################################################
+            switch (state) {
+                case "on" :
+                    execSync(`echo 0 > "${path.join(this.gpioPath,`/gpio${pin_off}`,`/value`)}" `)
+                    execSync(`echo 1 > "${path.join(this.gpioPath,`/gpio${pin_on}`,`/value`)}" `)
+                    break;
+                case "off" :
+                    execSync(`echo 0 > "${path.join(this.gpioPath,`/gpio${pin_on}`,`/value`)}" `)
+                    execSync(`echo 1 > "${path.join(this.gpioPath,`/gpio${pin_off}`,`/value`)}" `)
+                    break;
+                case "warn" :
+                    execSync(`echo 1 > "${path.join(this.gpioPath,`/gpio${pin_on}`,`/value`)}" `)
+                    execSync(`echo 1 > "${path.join(this.gpioPath,`/gpio${pin_off}`,`/value`)}" `)
+                    break;
+                case "dis" :
+                    execSync(`echo 0 > "${path.join(this.gpioPath,`/gpio${pin_on}`,`/value`)}" `)
+                    execSync(`echo 0 > "${path.join(this.gpioPath,`/gpio${pin_off}`,`/value`)}" `)
+                    break;
+            }
+        }catch (e){}
     }
 
     power(state : "on" | "off" | "dis" | "warn"){
@@ -66,37 +68,39 @@ export class HG680p {
         if (!this.isGpioExist)
             throw new Error("system not support gpio");
         //######################################################################
-        if (!fs.existsSync(`${path.join(this.gpioPath,`gpio${pin_on}`)}`)) {
-            execSync(`echo ${pin_on} > ${this.gpioPath}/export`);
-            execSync(`echo out > ${path.join(this.gpioPath,`/gpio${pin_on}`,`/direction`)}`);
-        }else{
-            execSync(`echo out > ${path.join(this.gpioPath,`/gpio${pin_on}`,`/direction`)}`);
-        }
-        if (!fs.existsSync(`${path.join(this.gpioPath,`gpio${pin_off}`)}`)){
-            execSync(`echo ${pin_off} > ${this.gpioPath}/export`);
-            execSync(`echo out > ${path.join(this.gpioPath,`/gpio${pin_off}`,`/direction`)}`);
-        }else{
-            execSync(`echo out > ${path.join(this.gpioPath,`/gpio${pin_off}`,`/direction`)}`);
-        }
-        //######################################################################
-        switch (state) {
-            case "on" :
-                execSync(`echo 0 > "${path.join(this.gpioPath,`/gpio${pin_off}`,`/value`)}" `)
-                execSync(`echo 1 > "${path.join(this.gpioPath,`/gpio${pin_on}`,`/value`)}" `)
-                break;
-            case "off" :
-                execSync(`echo 0 > "${path.join(this.gpioPath,`/gpio${pin_on}`,`/value`)}" `)
-                execSync(`echo 1 > "${path.join(this.gpioPath,`/gpio${pin_off}`,`/value`)}" `)
-                break;
-            case "warn" :
-                execSync(`echo 1 > "${path.join(this.gpioPath,`/gpio${pin_on}`,`/value`)}" `)
-                execSync(`echo 1 > "${path.join(this.gpioPath,`/gpio${pin_off}`,`/value`)}" `)
-                break;
-            case "dis" :
-                execSync(`echo 0 > "${path.join(this.gpioPath,`/gpio${pin_on}`,`/value`)}" `)
-                execSync(`echo 0 > "${path.join(this.gpioPath,`/gpio${pin_off}`,`/value`)}" `)
-                break;
-        }
+        try {
+            if (!fs.existsSync(`${path.join(this.gpioPath,`gpio${pin_on}`)}`)) {
+                execSync(`echo ${pin_on} > ${this.gpioPath}/export`);
+                execSync(`echo out > ${path.join(this.gpioPath,`/gpio${pin_on}`,`/direction`)}`);
+            }else{
+                execSync(`echo out > ${path.join(this.gpioPath,`/gpio${pin_on}`,`/direction`)}`);
+            }
+            if (!fs.existsSync(`${path.join(this.gpioPath,`gpio${pin_off}`)}`)){
+                execSync(`echo ${pin_off} > ${this.gpioPath}/export`);
+                execSync(`echo out > ${path.join(this.gpioPath,`/gpio${pin_off}`,`/direction`)}`);
+            }else{
+                execSync(`echo out > ${path.join(this.gpioPath,`/gpio${pin_off}`,`/direction`)}`);
+            }
+            //######################################################################
+            switch (state) {
+                case "on" :
+                    execSync(`echo 0 > "${path.join(this.gpioPath,`/gpio${pin_off}`,`/value`)}" `)
+                    execSync(`echo 1 > "${path.join(this.gpioPath,`/gpio${pin_on}`,`/value`)}" `)
+                    break;
+                case "off" :
+                    execSync(`echo 0 > "${path.join(this.gpioPath,`/gpio${pin_on}`,`/value`)}" `)
+                    execSync(`echo 1 > "${path.join(this.gpioPath,`/gpio${pin_off}`,`/value`)}" `)
+                    break;
+                case "warn" :
+                    execSync(`echo 1 > "${path.join(this.gpioPath,`/gpio${pin_on}`,`/value`)}" `)
+                    execSync(`echo 1 > "${path.join(this.gpioPath,`/gpio${pin_off}`,`/value`)}" `)
+                    break;
+                case "dis" :
+                    execSync(`echo 0 > "${path.join(this.gpioPath,`/gpio${pin_on}`,`/value`)}" `)
+                    execSync(`echo 0 > "${path.join(this.gpioPath,`/gpio${pin_off}`,`/value`)}" `)
+                    break;
+            }
+        }catch (e) {}
     }
 }
 
