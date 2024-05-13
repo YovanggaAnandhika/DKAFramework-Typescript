800e0dd3a861c2b20f62edb36978e311b1fae7de
Yovangga Anandhika <dka.tech.dev@gmail.com>
2024-05-14 04:26:47 +0800

    Update

---
+++ packages/Database/src/Firebase/Firestore/Config/DKAFirebaseConfigConstructor.ts
@@ -0,0 +1,7 @@
+import {FirebaseConfigConstructorConfiguration} from "./FirebaseConfigConstructor";
+
+
+export interface DKAFirebaseConfigConstructor {
+    credentials : FirebaseConfigConstructorConfiguration,
+    name ?: string | undefined
+}
